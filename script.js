3f806bddd4df5d872af8aad0e63ab1a868598dfd
adriankoder5 <adriankoder5@gmail.com>
2024-12-04 13:02:06 +0100

    save

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('Hello :3');
+const inpF = document.querySelector('#inpF');//inputfield
+const addB = document.querySelector('#addB');//addbutton
+const listP = document.querySelector('#listP');//listParent
+//const  = document.querySelector('#')
+
+addB.addEventListener('click', ()=> {
+    let inputV = inpF.value;
+    console.log(inputV);
+    let listItem = document.createElement('li');
+    listItem.textContent = inputV;
+    listP.appendChild(listItem);
+});
