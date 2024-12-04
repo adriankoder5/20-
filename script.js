acbd469548e309b44110cf0538c74b52186e1a45
adriankoder5 <adriankoder5@gmail.com>
2024-12-04 13:57:04 +0100

    ✓

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ addB.addEventListener('click', ()=> {
     let inputV = inpF.value;
     console.log(inputV);
     let listItem = document.createElement('li');
-    listItem.textContent = inputV;
+    listItem.textContent = inputV + ' ';
     listP.appendChild(listItem);
+    let crosB = document.createElement('BUTTON');//button for crossing over text
+    crosB.textContent = '✓';
+    listItem.appendChild(crosB);
+    crosB.addEventListener('click', ()=> {
+        console.log('det virker crosB');
+        if (listItem.style.textDecoration == 'line-through'){
+            listItem.style.textDecoration = 'inherit'
+            crosB.textContent ='✓';
+        }else {listItem.style.textDecoration = 'line-through'
+               crosB.textContent='X';
+        };
+    });
+    let killB = document.createElement('BUTTON');// Kill li
+    killB.textContent = 'slett';
+    listItem.appendChild(killB)
+    killB.addEventListener('click', ()=> {
+        console.log('delete inisiated')
+        listItem.remove();
+    })
 });
+
